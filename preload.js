d4df8492086446c8b806b0e9fde3e80ffaac9bde
唐瑞 <2478188327@qq.com>
2025-12-02 14:12:26 +0800

    初始化

---
+++ preload.js
@@ -0,0 +1,9 @@
+const {ipcRenderer, contextBridge} = require("electron")
+
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  // 除函数之外，我们也可以暴露变量
+  ping:()=>ipcRenderer.invoke('ping')
+})
